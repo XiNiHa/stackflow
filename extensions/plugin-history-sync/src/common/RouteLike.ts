4fd346a15ffef1d151244af081049a68e69e11b2
XiNiHa <me@xiniha.dev>
2024-07-01 19:29:19 +0900

    feat: add solid integration, first-party extensions, demo

---
+++ extensions/plugin-history-sync/src/common/RouteLike.ts
@@ -0,0 +1,16 @@
+import type { ActivityComponentType as ReactActivityComponentType } from "@stackflow/react";
+import type { ActivityComponentType as SolidActivityComponentType } from "@stackflow/solid";
+
+type Params<K> = K extends ReactActivityComponentType<infer U>
+  ? U
+  : K extends SolidActivityComponentType<infer U>
+    ? U
+    : Record<string, unknown>;
+
+export type Route<K> = {
+  path: string;
+  decode?: (params: Record<string, string>) => Params<K> | null;
+  encode?: (params: Params<K>) => Record<string, string>;
+};
+
+export type RouteLike<T> = string | string[] | Route<T> | Route<T>[];
